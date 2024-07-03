c1dbb74c8951bdcf066073e8a47039630d38e920
Jesse Mäkinen <jesse.j.makinen@gmail.com>
2024-07-04 00:04:23 +0300

    Fixed a nag about async_forward_entry_setup

--- custom_components/posti_tracking/__init__.py
+++ custom_components/posti_tracking/__init__.py
@@ -15,9 +15,7 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
     hass_data["unsub_options_update_listener"] = unsub_options_update_listener
     hass.data[DOMAIN][entry.entry_id] = hass_data
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
 
     return True
 
